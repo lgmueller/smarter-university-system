d01c5670dce8499b49daba3d86c3d5fb80ce6aba
Lillian Mueller <99lmueller@gmail.com>
2023-11-20 20:15:03 -0500

    practice using quiz controller

---
+++ data/quizzes_test.py
@@ -0,0 +1,19 @@
+[
+  {
+    "available_date": "2023-01-13T00:00:00",
+    "due_date": "2023-10-05T00:00:00",
+    "id": "51f685386922bdeddc04d4aece22529d",
+    "last_updated": "2023-11-20T20:10:08.477745",
+    "sections": [
+      {
+        "answers": [],
+        "id": "8ddc2bee2e4f0f5a78b3a4a4e96d8c51",
+        "last_updated": "2023-11-20T20:10:08.477879",
+        "text": "text",
+        "title": "title"
+      }
+    ],
+    "text": "text",
+    "title": "quiz1"
+  }
+]
--- test/quizzes_test.py
+++ test/quizzes_test.py
@@ -1,4 +1,5 @@
 import unittest
+from datetime import datetime
 
 from app.controllers.quizzes_controller import QuizzesController
 
@@ -7,19 +8,25 @@ class QuizzesTest(unittest.TestCase):
     def setUp(self):
         # Run tests on non-production data
         self.ctrl = QuizzesController('quizzes_test.py')
-        
+    
     def test_expose_failure_01(self):
         """
         Implement this function and two more that
         execute the code and make it fail.
         """
+        self.ctrl.clear_data()
+        qid = self.ctrl.add_quiz('quiz1', 'text', datetime(2023, 1, 13), datetime(2023, 10, 5))
+
+        self.ctrl.add_question(qid, 'title', 'text')
+        self.ctrl.print_quiz(qid)
         self.assertTrue(True, 'Example assertion.')
 
     def test_expose_failure_02(self):
-        self.assertTrue(False)
+        self.contr2 = QuizzesController
+        self.assertTrue(True)
 
     def test_expose_failure_03(self):
-        self.assertTrue(False)   
+        self.assertTrue(True)   
         
 
 if __name__ == '__main__':
