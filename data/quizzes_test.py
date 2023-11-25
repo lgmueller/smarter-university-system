5d9a4c87b320e34736524d2dc9c4fcbb8fe62a1e
Amrita Prakash <amyp1@umd.edu>
2023-11-25 16:06:53 -0500

    3 Unit Tests added (failures=1, errors=2)

--- data/quizzes_test.py
+++ data/quizzes_test.py
@@ -1,108 +1,23 @@
 [
   {
-    "id": "70e49b08483414a587a2fcb8bc274b19",
-    "last_updated": "2023-11-21T00:19:36.380267",
+    "id": "379460ccb19266d4dfced05283f488de",
+    "last_updated": "2023-11-25T15:57:58.977515",
     "sections": [
       {
         "answers": [
           {
-            "id": "6d3a4fcdaffda0b98a50d62213981cdf",
-            "is_correct": true,
-            "text": "this is ans yes to ques2-1"
-          },
-          {
-            "id": "25bb8cfe896ccfec1511de720393cbe0",
-            "is_correct": false,
-            "text": "this is ans no to ques2-1"
-          }
-        ],
-        "id": "a87da69cd16aae78f97d57a7f0ea6bb6",
-        "last_updated": "2023-11-21T00:19:36.393178",
-        "text": "details of ques 2-1",
-        "title": "Title of Ques 2-1"
-      }
-    ],
-    "text": "this is quiz 2",
-    "title": "Title of Quiz2"
-  },
-  {
-    "id": "cb33521da2fc32f734da1bdcee13b999",
-    "last_updated": "2023-11-21T00:19:36.470061",
-    "sections": [],
-    "text": "this is quiz 3",
-    "title": "Title of Quiz3"
-  },
-  {
-    "id": "b707fea6e2cef74694816e9d3c3321ca",
-    "last_updated": "2023-11-21T00:23:33.339651",
-    "sections": [
-      {
-        "answers": [
-          {
-            "id": "da1bf083ad3e2d766a55e55c50f6721f",
+            "id": "159721f52e3519b343fbede44a36ae2b",
             "is_correct": true,
             "text": "this is ans yes to ques3-1"
           },
           {
-            "id": "81b31f17a7b296e8d3037f475f3155fc",
+            "id": "300141aad6d8ce797e872002e2c38552",
             "is_correct": false,
             "text": "this is ans no to ques3-1"
           }
         ],
-        "id": "2deafc817df92b681ea264bea30d6837",
-        "last_updated": "2023-11-21T00:23:33.348471",
-        "text": "details of ques 3-1",
-        "title": "Title of Ques 3-1"
-      }
-    ],
-    "text": "this is quiz 3",
-    "title": "Title of Quiz3"
-  },
-  {
-    "id": "6f31dc243a0cc7574878c56b170ee43e",
-    "last_updated": "2023-11-21T00:26:58.094365",
-    "sections": [
-      {
-        "answers": [
-          {
-            "id": "94c1d69c30979e711972fb56bbd665b4",
-            "is_correct": true,
-            "text": "this is ans yes to ques3-1"
-          },
-          {
-            "id": "397615c30d2e656b121b49c23a603afa",
-            "is_correct": false,
-            "text": "this is ans no to ques3-1"
-          }
-        ],
-        "id": "8ed444951e373b20ccbeec3976243c77",
-        "last_updated": "2023-11-21T00:26:58.102365",
-        "text": "details of ques 3-1",
-        "title": "Title of Ques 3-1"
-      }
-    ],
-    "text": "this is quiz 3",
-    "title": "Title of Quiz3"
-  },
-  {
-    "id": "002d47bbe8605b755ead595596b0b568",
-    "last_updated": "2023-11-21T00:27:28.630638",
-    "sections": [
-      {
-        "answers": [
-          {
-            "id": "14b9b07aa2114dda18eabcc7dbe74fd6",
-            "is_correct": true,
-            "text": "this is ans yes to ques3-1"
-          },
-          {
-            "id": "5ee045919d55ca00da679a8fd5dd8d8a",
-            "is_correct": false,
-            "text": "this is ans no to ques3-1"
-          }
-        ],
-        "id": "df9c7dded8d10ac98555f599cc28600c",
-        "last_updated": "2023-11-21T00:27:28.638811",
+        "id": "54ac95322eb2c3dd947043db524e7c5f",
+        "last_updated": "2023-11-25T15:57:58.996827",
         "text": "details of ques 3-1",
         "title": "Title of Ques 3-1"
       }
--- test/quizzes_test.py
+++ test/quizzes_test.py
@@ -9,42 +9,38 @@ class QuizzesTest(unittest.TestCase):
         self.ctrl = QuizzesController('quizzes_test.py')
     
     def test_expose_failure_01(self):
-        """
-        check 1
-        """
+        '''
+        Error in generation of quiz ID because system is not able to convert the input datatype 
+        (Ex. none, bool, any other type) into string 
+        '''
         self.ctrl.clear_data()
-        quiz_id = self.ctrl.add_quiz('Title of Quiz1', 'this is quiz 1', '2023 09 13', '2023 09 5')
-        quiz_list = self.ctrl.get_quiz_by_id(quiz_id)
-        self.assertIsNotNone(quiz_list,'Quiz1 created')
+        quiz_id = self.ctrl.add_quiz(None, 'this is quiz 1', '2023 09 13', '2023 09 5')
+        self.assertIsNone(quiz_id,'Quiz id cannot be generated as None type cannot be converted to string')
        
       
     def test_expose_failure_02(self):
         """
-        check 2
+        Function call input arguments extra or missing
         """
         self.ctrl.clear_data()
         quiz_id = self.ctrl.add_quiz('Title of Quiz2', 'this is quiz 2', '2023 10 30', '2023 10 1')
-        ques_id = self.ctrl.add_question(quiz_id, 'Title of Ques 2-1', 'details of ques 2-1')
-        ques_list = self.ctrl.get_question_by_id(ques_id)
-        self.assertIsNotNone(ques_list,'question 2-1 created')
-        ans_id = self.ctrl.add_answer(ques_id,'this is ans yes to ques2-1',is_correct= True)
-        ans_id = self.ctrl.add_answer(ques_id,'this is ans no to ques2-1',is_correct= False)
-        self.assertIsNotNone(ans_id, 'Answers for question 1 created')
+        ques_id = self.ctrl.add_question(quiz_id, 'Title of Ques 2-1', 'question text')
+        ans_id = self.ctrl.add_answer(ques_id,'this is ans yes to ques2-1',True, is_correct=True)
+        self.assertIsNone(ans_id, 'Answers are not created, too many arguments')
 
       
     def test_expose_failure_03(self):
-        """
-        check 3
-        """
-        #self.ctrl.clear_data()
+        '''
+        Function return type error
+        '''
+        self.ctrl.clear_data()
         quiz_id = self.ctrl.add_quiz('Title of Quiz3', 'this is quiz 3', '2023 11 30', '2023 11 1')
         ques_id = self.ctrl.add_question(quiz_id, 'Title of Ques 3-1', 'details of ques 3-1')
         self.ctrl.add_answer(ques_id,'this is ans yes to ques3-1',is_correct= True)
         self.ctrl.add_answer(ques_id,'this is ans no to ques3-1',is_correct= False)
+        self.ctrl.print_quiz(quiz_id)
         quiz_list = self.ctrl.get_quizzes
-        self.ctrl.print_quiz(quiz_list)
-        self.assertTrue(True)   
-        
+        self.assertIs(type(quiz_list),list, 'get_quizzes is not returning a list')
 
 if __name__ == '__main__':
     unittest.main()
